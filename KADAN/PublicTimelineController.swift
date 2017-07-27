e694bfd8a34b963812c03199812c3710bcb91e55
Aodrey <Tomoko@gen-no-MacBook.local>
2017-07-28 00:03:59 +0900

    Initial Commit

---
+++ KADAN/PublicTimelineController.swift
@@ -0,0 +1,12 @@
+//
+//  PublicTimelineController.swift
+//  KADAN
+//
+//  Created by Aodrey on 2017/07/27.
+//  Copyright © 2017年 GauLabo. All rights reserved.
+//
+
+import UIKit
+class PublicTimelineController: UITableViewController {
+    
+}
